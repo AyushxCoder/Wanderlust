d657b0a3697e93f32d25201279827e9f1c008069
Ayush Jain <162486092+AyushxCoder@users.noreply.github.com>
2025-06-11 12:01:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   const allListings = await Listing.find();
   res.render("./listings/index.ejs", {allListings});
 });
