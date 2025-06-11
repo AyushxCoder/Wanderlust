959a86c8d7d6ed8c03b989837804b82146b46729
Ayush Jain <ayush99070@gmail.com>
2025-06-11 09:19:37 +0530

    Initial commit

--- app.js
+++ app.js
@@ -85,6 +85,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/", (req, res) => {
+  res.render("listings/index.ejs");
+});
+
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
